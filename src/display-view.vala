GNOME/connections

commit 7f1e29e17988b964f5c0069bcff3b566e50b6865

display-view: Account for offset of event_box child when relaying events
The VNC backend experiences an issue with pointer events being offset
from their intended position whenever there is excess space around
the widget. This is due to the (event_box-relative) events not being
adjusted before being fed into the child widget.

https://gitlab.gnome.org/GNOME/gnome-boxes/-/issues/157
https://gitlab.gnome.org/GNOME/gnome-boxes/-/issues/239

src/display-view.vala
@@ -101,8 +101,23 @@ namespace Connections {
             if (event.type == EventType.GRAB_BROKEN)
                 return false;
 
-            if (event_box.get_child () != null)
-                event_box.get_child ().event (event);
+            if (event_box.get_child () != null) {
+                var child = event_box.get_child ();
+                var offset_x = (get_allocated_width () - child.get_allocated_width ()) / 2.0;
+                var offset_y = (get_allocated_height () - child.get_allocated_height ()) / 2.0;
+
+                switch (event.get_event_type ()) {
+                case Gdk.EventType.MOTION_NOTIFY:
+                    event.motion.x -= offset_x;
+                    event.motion.y -= offset_y;
+                    break;
+
+                default:
+                    break;
+                }
+
+                child.event (event);
+            }
 
             return false;
         } 

commit 9ae9c0bfb60770a60872feb7fb98a69c97b9d511

display-view: Fix position of cursor for original scale
The position of cursor was offset by certain amount when
the display had original scale. This was caused by
the commit 7f1e29e17988b964f5c0069bcff3b566e50b6865
(display-view: Account for offset of event_box child when relaying events)
which seems to have been fixed on another place in between.
The issue was present in both backends VNC and RDP.

src/display-view.vala
@@ -104,23 +104,8 @@ namespace Connections {
             if (event.type == EventType.GRAB_BROKEN)
                 return false;
 
-            if (event_box.get_child () != null) {
-                var child = event_box.get_child ();
-                var offset_x = (get_allocated_width () - child.get_allocated_width ()) / 2.0;
-                var offset_y = (get_allocated_height () - child.get_allocated_height ()) / 2.0;
-
-                switch (event.get_event_type ()) {
-                case Gdk.EventType.MOTION_NOTIFY:
-                    event.motion.x -= offset_x;
-                    event.motion.y -= offset_y;
-                    break;
-
-                default:
-                    break;
-                }
-
-                child.event (event);
-            }
+            if (event_box.get_child () != null)
+                event_box.get_child ().event (event);
 
             return false;
         } 